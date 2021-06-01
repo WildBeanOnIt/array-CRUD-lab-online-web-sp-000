9e7e63e29f24c6470f0e2d9cac43b8f7f1b5d58d
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-06-01 23:05:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array.push("testFive")
+  create_an_array << testFive
 end
 
 def add_element_to_start_of_array(array, element)
